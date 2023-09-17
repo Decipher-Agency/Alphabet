7bb337d63993bb67e821835c8dd800ffa4fcff4e
Decipher <129142692+Decipher-Agency@users.noreply.github.com>
2023-09-18 00:30:30 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -14,37 +14,43 @@ document.addEventListener("DOMContentLoaded", function () {
 
 // ! IMAGE JAVASCRIPT IN ABOUT SECTION
 
-const images = [
-  "./IMG/animation aquapirate.png" ,
-  "./IMG/image-1.png" ,
-  "./IMG/image-2.png" ,
-  "./IMG/image-3.png" ,
-  "./IMG/image-14.png" ,
-  "./IMG/image-5.png" ,
-  "./IMG/image-13.png" ,
-  "./IMG/image-7.png" ,
-  "./IMG/image-8.png" ,
-];
-const fadeInDuration = 1500; // 1,5 second
-const fadeOutDuration = 1500; // 1,5 second
-const pauseDuration = 3000; // 3 seconds
-const totalImages = images.length;
-let currentIndex = 0;
+    const images = [
+      "./IMG/animation aquapirate.png" ,
+      "./IMG/image-1.png" ,
+      "./IMG/image-2.png" ,
+      "./IMG/image-3.png" ,
+      "./IMG/image-14.png" ,
+      "./IMG/image-5.png" ,
+      "./IMG/image-13.png" ,
+      "./IMG/image-7.png" ,
+      "./IMG/image-8.png" ,
+    ];
+    const fadeInDuration = 1500; // 1 second
+    const fadeOutDuration = 1500; // 1 second
+    const pauseDuration = 3000; // 2 seconds
+    const totalImages = images.length;
+    let currentIndex = 0;
 
-function fadeInImage(index) {
-  document.querySelector('.fade-image').src = images[index];
-  document.querySelector('.fade-image').style.opacity = '1';
-  setTimeout(() => {
-    fadeOutImage(index);
-  }, pauseDuration);
-}
+    const imageElement = document.getElementById('image');
 
-function fadeOutImage(index) {
-  document.querySelector('.fade-image').style.opacity = '0';
-  setTimeout(() => {
-    currentIndex = (currentIndex + 1) % totalImages;
+    function fadeInImage(index) {
+      imageElement.src = images[index];
+      imageElement.style.opacity = '1';
+      setTimeout(() => {
+        if (imageElement.style.opacity === '1') {
+          fadeOutImage(index);
+        }
+      }, pauseDuration);
+    }
+
+    function fadeOutImage(index) {
+      imageElement.style.opacity = '0';
+      setTimeout(() => {
+        if (imageElement.style.opacity === '0') {
+          currentIndex = (currentIndex + 1) % totalImages;
+          fadeInImage(currentIndex);
+        }
+      }, fadeOutDuration);
+    }
+
     fadeInImage(currentIndex);
-  }, fadeOutDuration);
-}
-
-fadeInImage(currentIndex);
